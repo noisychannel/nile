973263eecf7dafa4d31478f906113f660d24c2e4
Gaurav Kumar <gaurav.bison@gmail.com>
2015-05-14 16:33:03 -0500

    Function returns VariableIndex for integration with PRO

--- src/rnnContextRule.cc
+++ src/rnnContextRule.cc
@@ -69,15 +69,17 @@ struct RNNContextRule {
   Builder builder_rule_source;
   Builder builder_rule_target;
 
-  explicit RNNContextRule(Model &model) :
+  explicit RNNContextRule(Model &model, LookupParameters* p_w_s, LookupParameters* p_w_t) :
     builder_context_left(LAYERS, EMBEDDING_DIM, HIDDEN_DIM, &model),
     builder_context_right(LAYERS, EMBEDDING_DIM, HIDDEN_DIM, &model),
     builder_rule_source(LAYERS, EMBEDDING_DIM, HIDDEN_DIM, &model),
     builder_rule_target(LAYERS, EMBEDDING_DIM, HIDDEN_DIM, &model)
   {
     //TODO: What is the dimnesionality??
-    p_w_source = model.add_lookup_parameters(VOCAB_SIZE_SOURCE, {EMBEDDING_DIM});
-    p_w_target = model.add_lookup_parameters(VOCAB_SIZE_TARGET, {EMBEDDING_DIM});
+    p_w_source = p_w_s;
+    p_w_target = p_w_t;
+    //p_w_source = model.add_lookup_parameters(VOCAB_SIZE_SOURCE, {EMBEDDING_DIM});
+    //p_w_target = model.add_lookup_parameters(VOCAB_SIZE_TARGET, {EMBEDDING_DIM});
     p_R_cl = model.add_parameters({VOCAB_SIZE_SOURCE, HIDDEN_DIM});
     p_bias_cl = model.add_parameters({VOCAB_SIZE_SOURCE});
     p_R_cr = model.add_parameters({VOCAB_SIZE_SOURCE, HIDDEN_DIM});
@@ -147,14 +149,13 @@ struct RNNContextRule {
   // The embeddings are currently simply summed together to get the feature
   // vector for the hypothesis. This may change in the future.
   // TODO (gaurav)
-  std::vector<struct Context> currentContext;
-  std::vector<VariableIndex> currentEmbedding;
   VariableIndex BuildRuleSequenceModel(std::vector<struct Context> cSeq, Hypergraph& hg) {
+    //TODO; Is this count right ?
     const unsigned cSeqLen = cSeq.size() - 1;
     std::vector<VariableIndex> ruleEmbeddings;
     for( std::vector<struct Context>::const_iterator i = cSeq.begin(); i != cSeq.end(); ++i) {
-      currentContext = *i;
-      currentEmbedding = Recurrence(currentContext, hg);
+      Context currentContext = *i;
+      VariableIndex currentEmbedding = BuildRNNGraph(currentContext, hg);
       ruleEmbeddings.push_back(currentEmbedding);
     }
     //TODO: This may be buggy
@@ -229,49 +230,69 @@ std::vector<Context> getContexts(std::string t, vector<int> s) {
 }
 
 
-int main(int argc, char** argv) {
-  cnn::Initialize(argc, argv);
+VariableIndex getRNNRuleContext(std::string rawSrc, std::string targetHyp,
+  LookupParameters* p_w_source, LookupParameters* p_w_target,
+    Hypergraph hg, Model model) {
+//int main(int argc, char** argv) {
+  //1. Source sentence
+  //2. Target Hyp
+  //cnn::Initialize(argc, argv);
   // Read the source and the n-best file and create a sequence of contexts
-  std::cerr << "Reading source sentences from " << argv[1] << "...\n";
-  std::cerr << "Read n-best target hyps from " << argv[2] << "...\n";
+  //std::cerr << "Reading source sentences from " << argv[1] << "...\n";
+  //std::cerr << "Read n-best target hyps from " << argv[2] << "...\n";
   // Counts the number of lines
   //int tlc = 0;
   // Counts the number of tokens
   //int ttoks = 0;
   //vector< vector<int> > training, dev;
 
-  std::string sourceLine;
-  std::string targetHyp;
-  std::string currentSrcID;
+  std::vector<int> currentSrc = ReadSentence(rawSrc, &sourceD);
+  std::vector<Context> contexts = getContexts(targetHyp, currentSrc);
+  RNNContextRule<RNNBuilder> rnncr(model, p_w_source, p_w_target);
+  return rnncr.BuildRuleSequenceModel(contexts, hg);
+
+  //std::string sourceLine;
+  //std::string targetHyp;
+  //std::string currentSrcID;
+  //std::string rawSrc;
+  //vector<int> currentSrc;
+  //{
+    //ifstream sourceIn(argv[1]);
+    //ifstream nBestIn(argv[2]);
+    //assert(sourceIn);
+    //assert(nBestIn);
+    //while (getline(nBestIn, targetHyp)) {
+      ////++tlc;
+      //KbestHypothesis hyp = KbestHypothesis::parse(targetHyp);
+      //if (hyp.sentence_id != currentSrcID) {
+        //// We have a new n-best sequence
+        //// First get the new source sentence
+        //getline(sourceIn, rawSrc);
+        //currentSrc = ReadSentence(rawSrc, &sourceD);
+        //currentSrcID = hyp.sentence_id;
+      //}
+      //// targetHyp looks something like this
+      //// for |0-0| someone |1-1| to call you |2-3| or something |4-5| . |6-6|
+    //}
+  //}
+  //sourceD.Freeze();
+  //targetD.Freeze();
+  //VOCAB_SIZE_SOURCE = sourceD.size();
+  //VOCAB_SIZE_TARGET = targetD.size();
+  //// Read contexts and start recurrence
+  //Model model;
+  //Trainer* sgd = new SimpleSGDTrainer(&model);
+  //Hypergraph hg;
+}
+
+int main(int argc, char** argv) {
   std::string rawSrc;
-  vector<int> currentSrc;
-  {
-    ifstream sourceIn(argv[1]);
-    ifstream nBestIn(argv[2]);
-    assert(sourceIn);
-    assert(nBestIn);
-    while (getline(nBestIn, targetHyp)) {
-      //++tlc;
-      KbestHypothesis hyp = KbestHypothesis::parse(targetHyp);
-      if (hyp.sentence_id != currentSrcID) {
-        // We have a new n-best sequence
-        // First get the new source sentence
-        getline(sourceIn, rawSrc);
-        currentSrc = ReadSentence(rawSrc, &sourceD);
-        currentSrcID = hyp.sentence_id;
-      }
-      // targetHyp looks something like this
-      // for |0-0| someone |1-1| to call you |2-3| or something |4-5| . |6-6|
-      std::vector<Context> contexts = getContexts(targetHyp, currentSrc);
-    }
-  }
-  sourceD.Freeze();
-  targetD.Freeze();
-  VOCAB_SIZE_SOURCE = sourceD.size();
-  VOCAB_SIZE_TARGET = targetD.size();
-
+  std::string targetHyp;
+  //cnn::Dict sourceD;
+  //cnn::Dict targetD;
+  LookupParameters* p_w_source;
+  LookupParameters* p_w_target;
+  Hypergraph hg;
   Model model;
-  Trainer* sgd = new SimpleSGDTrainer(&model);
-  RNNContextRule<RNNBuilder> rnncr(model);
-
+  getRNNRuleContext(rawSrc, targetHyp, p_w_source, p_w_target, hg, model);
 }
