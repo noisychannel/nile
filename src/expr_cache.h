8f643e8d3ba4aa7019bf9b18e021f7b3238071cb
Gaurav Kumar <gaurav.bison@gmail.com>
2015-08-04 01:50:28 -0400

    Expression cache uses unordered maps now.

--- src/expr_cache.h
+++ src/expr_cache.h
@@ -1,14 +1,36 @@
 #pragma once
 #include <tuple>
-#include <map>
+#include <utility>
+#include <unordered_map>
 #include "cnn/expr.h"
 
+using namespace std;
 using namespace cnn::expr;
 
+typedef tuple<unsigned, unsigned> cache_key_t;
+
+struct cache_key_hash : public unary_function<cache_key_t, size_t> {
+  size_t operator()(const cache_key_t& k) const {
+    return get<0>(k) ^ get<1>(k);
+  }
+};
+
+struct cache_key_equal : public binary_function<cache_key_t, cache_key_t, bool> {
+  bool operator()(const cache_key_t& v0, const cache_key_t& v1) const {
+    return (
+      get<0>(v0) == get<0>(v1) && get<1>(v0) == get<1>(v1)
+    );
+  }
+};
+
+
+typedef unordered_map<const cache_key_t, Expression, cache_key_hash, cache_key_equal> cache_map_t;
+typedef unordered_map<const cache_key_t, vector<Expression>, cache_key_hash, cache_key_equal> cache_map_vec_t;
+
 struct ExpCache {
-  map<tuple<unsigned, unsigned>, vector<Expression> > srcExpCache;
-  map<tuple<unsigned, unsigned>, Expression> tPhraseCache;
-  map<tuple<unsigned, unsigned>, Expression> sPhraseCache;
-  map<unsigned, Expression> lContextCache;
-  map<unsigned, Expression> rContextCache;
+  cache_map_vec_t srcExpCache;
+  cache_map_t tPhraseCache;
+  cache_map_t sPhraseCache;
+  unordered_map<unsigned, Expression> lContextCache;
+  unordered_map<unsigned, Expression> rContextCache;
 };
